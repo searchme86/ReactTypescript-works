9895dbbe533a8a8957b1797a64d34ca331442425
searchme86 <searchme86@gmail.com>
2022-11-03 14:54:26 +0900

    ✨ test immer

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import React, { ChangeEvent, useState, useRef } from 'react';
 //   WhiteUnderlineButton,
 // } from './Buttons';
 // import Calc from './Calc';
+import produce from 'immer';
 
 export type CountryType = {
   no: number;
@@ -70,6 +71,41 @@ function App() {
     setY(y1);
     setResult(x1 + y1);
   };
+
+  let quiz = {
+    students: ['홍길동', '성춘향', '박문수', '변학도'],
+    quizlist: [
+      {
+        question: '한국 프로 야구팀이 아닌것은?',
+        options: [
+          { no: 1, option: '삼성 라이온즈' },
+          { no: 2, option: '기아 타이거즈' },
+          { no: 3, option: '두산 베어즈' },
+          { no: 4, option: 'LA 다저스' },
+        ],
+        answer: 4,
+      },
+      {
+        question: '2018년 크리스마스는 무슨 요일인가?',
+        options: [
+          { no: 1, option: '월' },
+          { no: 2, option: '화' },
+          { no: 3, option: '수' },
+          { no: 4, option: '목' },
+        ],
+        answer: 2,
+      },
+    ],
+  };
+
+  const quiz2 = produce(quiz, (draft) => {
+    draft.quizlist[0].options[0].option = 'LG트윈스';
+  });
+
+  console.log(quiz === quiz2);
+  console.log(quiz.quizlist === quiz2.quizlist);
+  console.log(quiz.quizlist[0] === quiz2.quizlist[0]);
+  console.log(quiz.students === quiz2.students);
 
   return (
     // <div className="container">

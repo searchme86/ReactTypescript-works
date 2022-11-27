3aca611463d758a11a509e95e7f442f01e08fc66
searchme86 <searchme86@gmail.com>
2022-11-27 20:59:51 +0900

    👏🏻 add reducrer

---
+++ src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,56 @@
+interface RepositoriesState {
+  loading: boolean;
+  error: string | null;
+  data: string[];
+}
+
+enum ActionType {
+  SEARCH_REPOSITORIES = 'search_repositories',
+  SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_succeess',
+  SEARCH_REPOSITORIES_ERROR = 'search_repositories_error',
+}
+
+interface SearchRepositoriesAction {
+  type: ActionType.SEARCH_REPOSITORIES;
+}
+
+interface SearchRepositoriesSuccessAction {
+  type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
+  payload: string[];
+}
+
+interface SearchRepositoriesErrorAction {
+  type: ActionType.SEARCH_REPOSITORIES_ERROR;
+  payload: string;
+}
+
+type Action =
+  | SearchRepositoriesAction
+  | SearchRepositoriesSuccessAction
+  | SearchRepositoriesErrorAction;
+
+const reducer = (
+  state: RepositoriesState,
+  action: Action
+): RepositoriesState => {
+  switch (action.type) {
+    case ActionType.SEARCH_REPOSITORIES:
+      return { loading: true, error: null, data: [] };
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+      return {
+        loading: false,
+        error: null,
+        data: action.payload,
+      };
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
+      return {
+        loading: false,
+        error: action.payload,
+        data: [],
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;

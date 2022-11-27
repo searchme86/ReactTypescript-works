b37c7a178aaf976f37a0dd0b90068dff3327e3a4
searchme86 <searchme86@gmail.com>
2022-11-27 21:59:16 +0900

    👏🏻 RepositoriesList components added

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { actionCreators } from '../state';
 
 function RepositoriesList() {
+  const [term, setTerm] = useState('');
+  const dispatch = useDispatch();
+
+  const InputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTerm(event.target.value);
+  };
+
+  const onSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    dispatch(actionCreators.searchRepositories(term) as any);
+  };
+
   return (
     <div>
-      <form action="">
-        <input type="text" />
+      <form action="" onSubmit={onSubmitHandler}>
+        <input type="text" onChange={InputHandler} />
         <button>Search</button>
       </form>
     </div>
   );
 }
+
+//dispatch(actionCreators.searchRepositories(term) as any
 
 export default RepositoriesList;
